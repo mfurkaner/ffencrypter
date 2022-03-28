053f37ab0d7856940e20ffca9525f8f1ffaca61f
Furkan <mfurkaner.mfe@gmail.com>
2022-03-29 00:33:11 +0300

    added mangler.cpp to run scripts

--- scripts/run_test.bat
+++ scripts/run_test.bat
@@ -1,5 +1,5 @@
 @echo off&&cls
 cd ../src
-g++ -g -g tests.cpp hash.cpp encryptengine.cpp application.cpp ../test/test_hash.cpp -o ../test.exe -std=c++11
+g++ -g -g tests.cpp hash.cpp encryptengine.cpp mangler.cpp application.cpp ../test/test_hash.cpp -o ../test.exe -std=c++11
 cd ..
 test.exe
