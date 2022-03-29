22ea2e98abb58462530734bf2a7c5caccd09e78e
Furkan <mfurkaner.mfe@gmail.com>
2022-03-29 22:25:46 +0300

    added filehandler.cpp

--- inc/filehandler.hpp
+++ inc/filehandler.hpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <sstream>
-#include <fstream>
+
 
 #ifndef FILE_HANDLER
 #define FILE_HANDLER
@@ -8,59 +6,16 @@
 
 class FileHandler{
     std::string filepath;
-
 public:
 
     void setFilePath(const std::string& path){ filepath = path;}
 
-    bool getTextFromFile(std::string& text){
-        text = "";
-        std::ifstream fin;
-        fin.open(filepath, std::ios::in | std::ios::binary);
-        std::string line;
-        if(fin.fail()) return false;
-        while( !fin.eof() ){
-            std::getline(fin, line);
-            text += line + ( fin.eof() ? "" : "\n" );
-        }
-        fin.close();
-        return true;
-    }
-    bool writeTextToFile(const std::string& text){
-        std::ofstream fout;
-        fout.open(filepath, std::ios::out | std::ios::binary);
-        if(fout.fail()) return false;
-        fout << text;
-        fout.close();
-        return true;
-    } 
+    bool getTextFromFile(std::string& text);
+    bool writeTextToFile(const std::string& text);
 
-    static int printOnlyDifferences(const std::string& t1, const std::string& t2){
-        int count = 0;
-        for(uint32_t i = 0; i < t1.size() && i < t2.size() ; i++){
-            if( t1.at(i) != t2.at(i) ){
-                std::cout << "They differ at " << i << " : t1=" << t1.at(i) << " t2=" << t2.at(i) << std::endl; 
-                count++;
-            }
-        }
-        return count;
-    }
-    
-    static void printDifferences(const std::string& t1, const std::string& t2){
-        int count = printOnlyDifferences(t1, t2);
-        std::cout << "Size of t1 : " << t1.size() << "  size of t2 : " << t2.size() << std::endl;
-        std::cout << "Total diff : " << count << std::endl;
-    }
-
-    static void printDifferencesOfFiles(const std::string& f1, const std::string& f2){
-        FileHandler fh1, fh2;
-        std::string s1, s2;
-        fh1.setFilePath(f1);
-        fh1.getTextFromFile(s1);
-        fh2.setFilePath(f2);
-        fh2.getTextFromFile(s2);
-        printDifferences(s1, s2);
-    }
+    static int printOnlyDifferences(const std::string& t1, const std::string& t2);
+    static void printDifferences(const std::string& t1, const std::string& t2);
+    static void printDifferencesOfFiles(const std::string& f1, const std::string& f2);
 
 };
 
---
+++ src/filehandler.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include "../inc/filehandler.hpp"
+
+bool FileHandler::getTextFromFile(std::string& text){
+    text = "";
+    std::ifstream fin;
+    fin.open(filepath, std::ios::in | std::ios::binary);
+    std::string line;
+    if(fin.fail()) return false;
+    while( !fin.eof() ){
+        std::getline(fin, line);
+        text += line + ( fin.eof() ? "" : "\n" );
+    }
+    fin.close();
+    return true;
+}
+
+bool FileHandler::writeTextToFile(const std::string& text){
+    std::ofstream fout;
+    fout.open(filepath, std::ios::out | std::ios::binary);
+    if(fout.fail()) return false;
+    fout << text;
+    fout.close();
+    return true;
+} 
+
+int FileHandler::printOnlyDifferences(const std::string& t1, const std::string& t2){
+    uint32_t count = 0;
+    for(uint32_t i = 0; i < t1.size() && i < t2.size() ; i++){
+        if( t1.at(i) != t2.at(i) ){
+            std::cout << "They differ at " << i << " : t1=" << t1.at(i) << " t2=" << t2.at(i) << std::endl; 
+            count++;
+        }
+    }
+    return count;
+}
+
+void FileHandler::printDifferences(const std::string& t1, const std::string& t2){
+    uint32_t count = printOnlyDifferences(t1, t2);
+    std::cout << "Size of t1 : " << t1.size() << "  size of t2 : " << t2.size() << std::endl;
+    std::cout << "Total diff : " << count << std::endl;
+}
+
+void FileHandler::printDifferencesOfFiles(const std::string& f1, const std::string& f2){
+    FileHandler fh1, fh2;
+    std::string s1, s2;
+    fh1.setFilePath(f1);
+    fh1.getTextFromFile(s1);
+    fh2.setFilePath(f2);
+    fh2.getTextFromFile(s2);
+    printDifferences(s1, s2);
+}
